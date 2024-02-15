0009ab3e01e90c1d84ee1c59813a5930e06425bb
Andy ramaroson <lemurianagency@gmail.com>
2024-02-15 22:03:11 +0100

    [site-config]update information twitter

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,12 +1,12 @@
 export const SiteConfig = {
-  title: "base-v2",
-  description: "A template description for base-v2",
+  title: "Lemurian ",
+  description: "A template description for product:base-v2",
   prodUrl: "https://base-v2.vercel.app",
   domain: "andyramaroson.com",
   appIcon: "/images/icon.png",
   company: {
     name: "Lemurian Inc.",
-    address: "1234 Main St, New York, NY 10001",
+    address: "12 avenue des champs-elysées paris",
   },
   brand: {
     primary: "#007291",
@@ -18,7 +18,7 @@ export const SiteConfig = {
   maker: {
     image: "https://res.cloudinary.com/dbtnehfrf/image/upload/c_thumb,w_200,g_face/v1686826750/t%C3%A9l%C3%A9chargement_ocizbr.png",
     website: "https://andyramaroson.com",
-    twitter: "https://twitter.com/andyrama",
+    twitter: "#",
     name: "Andy",
   },
   auth: {

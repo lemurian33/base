d2ca5d83ed0484f76f011b35dd69422776fbe38a
Andy ramaroson <lemurianagency@gmail.com>
2024-02-15 15:16:43 +0100

    [site-config]update information and domain

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-Welcome to [NOW.TS](https://nowts.app) directory.
-
-## Setup the project
-
-Please follow the [NOW.TS Course](https://codeline.app/courses/clqn8pmte0001lr54itcjzl59/lessons/clqn8pz990003112iia11p7uo) to setup the project.
+Welcome to [Base](https://base-v2.app) directory.
--- src/site-config.ts
+++ src/site-config.ts
@@ -1,25 +1,25 @@
 export const SiteConfig = {
-  title: "Now.ts",
-  description: "A template description for Now.ts",
-  prodUrl: "https://nowts.app",
-  domain: "nowts.app",
+  title: "base-v2",
+  description: "A template description for base-v2",
+  prodUrl: "https://base-v2.vercel.app",
+  domain: "andyramaroson.com",
   appIcon: "/images/icon.png",
   company: {
-    name: "NowTS Inc.",
+    name: "Base-v2 Inc.",
     address: "1234 Main St, New York, NY 10001",
   },
   brand: {
     primary: "#007291",
   },
   email: {
-    from: "NowTS <contact@nowts.app>",
-    contact: "contact@nowts.app",
+    from: "Base-v2 <contact@base-v2.app>",
+    contact: "contact@base-v2.app",
   },
   maker: {
     image: "https://melvynx.com/images/me/twitter-en.jpg",
-    website: "https://melvynx.com",
-    twitter: "https://twitter.com/melvyn_me",
-    name: "Melvynx",
+    website: "https://andyramaroson.com",
+    twitter: "https://twitter.com/andyrama",
+    name: "Andy",
   },
   auth: {
     password: false as boolean,
